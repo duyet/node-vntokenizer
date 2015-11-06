3ad7d1f7b3b2909a457dbc5a8f966415b06cc1c1
Van-Duyet Le <lvduit08@gmail.com>
2015-11-07 00:21:07 +0700

    Update regex tokenizer

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 'use strict';
 
 (function() {
-    var root = this;
-    var has_require = typeof require !== 'undefined';
-    
-    if (typeof _ === 'undefined') {
-        if (has_require) {
-            var util = require('util');
-            var fs = require('fs');
-            var _ = require('lodash');
-        } else {
-
-        }
-    }
-
-    var vnTokenizer = function(options) {
-        var options = options || {};
-    };
-
-    // Exports
-    if( typeof exports !== 'undefined' ) {
-        if( typeof module !== 'undefined' && module.exports ) {
-            exports = module.exports = vnTokenizer;
-        }
-        exports.vnTokenizer = vnTokenizer;
-    } else {
-        root.vnTokenizer = vnTokenizer;
-    }
+    module.exports = require('./lib/tokenizer');
 }).call(this);
